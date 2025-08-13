fb5d5d162a8ea6c5cdb6e7018c01b6b55e5ada20
Rodrigo Marquez <a@Rodrigo-Marquez-MacBook-Air.local>
2025-08-13 17:54:01 -0300

    Se agrega el responsive con los cambios de SearchBar, tanto de tsx como del css

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,6 +9,7 @@ const logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Internat
 
 export default function SearchBar({ onSearch }: SearchBarProps) {
   const [query, setQuery] = useState("");
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
@@ -16,41 +17,43 @@ export default function SearchBar({ onSearch }: SearchBarProps) {
 
   const handleSearchClick = () => {
     onSearch(query);
+    if (menuOpen) setMenuOpen(false);
   };
 
   const handleClear = () => {
     setQuery("");
     onSearch("");
+    if (menuOpen) setMenuOpen(false);
   };
 
   return (
-    <header className="main-header">
-      <ul>
-        <li>
-          <a href="/" className="logo">
+    <>
+      {menuOpen && <div className="overlay" onClick={() => setMenuOpen(false)}></div>}
+
+      <div className="top-bar">
+        <a href="/" className="logo">
           <img src={logo} alt="Logo" />
-          </a>
-        </li>
-        <li>
-          <input
-            type="text"
-            placeholder="Buscar por nombre o ID"
-            value={query}
-            onChange={handleInputChange}
-          />
+        </a>
+        <div className="menu-toggle" onClick={() => setMenuOpen(true)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
 
+      <header className={`main-header ${menuOpen ? 'open' : ''}`}>
+        <a href="/" className="logo">
+          <img src={logo} alt="Logo" />
+        </a>
+
+        <div className="search-controls">
+          <input type="text" placeholder="Buscar por nombre o ID" value={query} onChange={handleInputChange}/>
           <button onClick={handleClear}>❌</button>
-
-          <svg
-            className="magnifying-glass"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            onClick={handleSearchClick}
-          >
+          <svg className="magnifying-glass" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" onClick={handleSearchClick}>
             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
           </svg>
-        </li>
-      </ul>
-    </header>
+        </div>
+      </header>
+    </>
   );
 }
